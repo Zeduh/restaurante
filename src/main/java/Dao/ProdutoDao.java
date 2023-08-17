5c31b856557bddaea69248866e4c04723d5446e3
Zeduh <jeduardocury@gmail.com>
2023-08-17 15:36:30 -0300

    Adicionado DAO na controller, agora DAO é quem está criando a lista estáticamente, ainda sem conexão com bando de dados. Adicionado também a descrição de categoria.

--- src/main/java/Dao/ProdutoDao.java
+++ src/main/java/Dao/ProdutoDao.java
@@ -2,16 +2,20 @@ package Dao;
 
 import model.Categoria;
 import model.Produto;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class ProdutoDao {
 
     private static List<Produto> produtos = new ArrayList<>();
 
     public List<Produto> lista(){
+        produtos.clear();
         produtos.add(new Produto(1, "salada", "Salada de alface e tomate", Categoria.COMIDA, BigDecimal.TEN));
         produtos.add(new Produto(2, "Frango grelhado", "Acompanha arroz", Categoria.COMIDA, BigDecimal.ONE));
         produtos.add(new Produto(3, "Refrigerante", "Agua tônica sem açucar", Categoria.COMIDA, BigDecimal.ZERO));
--- src/main/java/br/com/joseduardo/restaurante/controller/HomeController.java
+++ src/main/java/br/com/joseduardo/restaurante/controller/HomeController.java
@@ -1,24 +1,21 @@
 package br.com.joseduardo.restaurante.controller;
 
 import Dao.ProdutoDao;
-import model.Produto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import java.util.List;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 
+    private ProdutoDao dao = new ProdutoDao();
+
     @GetMapping("/lista")
     public String lista(Model model){
 
-        ProdutoDao dao = new ProdutoDao();
-        List<Produto> produtos = dao.lista();
-
-        model.addAttribute("produtos", produtos);
+        model.addAttribute("produtos", dao.lista());
 
         return "produtos";
     }
