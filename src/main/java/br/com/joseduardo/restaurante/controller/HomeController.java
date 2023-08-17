5fe6e356e600e5c19e25a23d516bc321c9031c41
Zeduh <jeduardocury@gmail.com>
2023-08-17 15:00:46 -0300

    Implementado produto DAO juntamente com função para listar produtos, ainda sem conexão com banco de dados.

--- src/main/java/br/com/joseduardo/restaurante/controller/HomeController.java
+++ src/main/java/br/com/joseduardo/restaurante/controller/HomeController.java
@@ -1,15 +1,11 @@
 package br.com.joseduardo.restaurante.controller;
 
 import Dao.ProdutoDao;
-import model.Categoria;
 import model.Produto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
