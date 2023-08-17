0d1df17716a2c4ef2f531221f25af7a06040ba50
Zeduh <jeduardocury@gmail.com>
2023-08-17 20:18:12 -0300

    Criado DTOs para formulário e listagem completa de produtos.

--- src/main/java/br/com/joseduardo/restaurante/controller/ProdutoController.java
+++ src/main/java/br/com/joseduardo/restaurante/controller/ProdutoController.java
@@ -1,10 +1,18 @@
 package br.com.joseduardo.restaurante.controller;
 import Dao.ProdutoDao;
+import model.Categoria;
+import model.Produto;
+import model.dto.ProdutoFormImputDto;
+import model.dto.ProdutoOutputDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/produto")
@@ -14,20 +22,26 @@ public class ProdutoController {
 
     @GetMapping("/lista")
     public String lista(Model model){
-
-        model.addAttribute("produtos", dao.lista());
-
+        List<ProdutoOutputDto> produtosDto = dao
+                .lista()
+                .stream()
+                .map(produto -> new ProdutoOutputDto(produto))
+                .collect(Collectors.toList());
+        model.addAttribute("produtos", produtosDto);
         return "produtos";
     }
 
     @GetMapping("/form")
-    public String form(){
+    public String form(Model model) {
+        model.addAttribute("categorias", Categoria.values());
         return "form";
     }
 
     @PostMapping("/cadastra")
-    public void cadastra(){
-        System.out.println("**** Metodo de cadastro");
+    public String cadastra(ProdutoFormImputDto produtoFormImputDto, Model model){
+        Produto produto = produtoFormImputDto.toProduto();
+        this.dao.cadastra(produto);
+        return lista(model);
     }
 
 }
---
+++ src/main/java/model/dto/ProdutoOutputDto.java
@@ -0,0 +1,30 @@
+package model.dto;
+
+import model.Produto;
+
+import java.math.BigDecimal;
+
+public class ProdutoOutputDto {
+
+    private Integer id;
+    private String nome;
+    private BigDecimal preco;
+
+    public ProdutoOutputDto(Produto produto) {
+        this.id = produto.getId();
+        this.nome = produto.getNome();
+        this.preco = produto.getPreco();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+}
