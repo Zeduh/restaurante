fb5c32eed290fab247148b77a19399a8c8a340b1
Zeduh <jeduardocury@gmail.com>
2023-08-18 12:56:28 -0300

    Finalizado cadastro de produto, implementado também validação dos campos para evitar cadastros inconsistentes.

--- src/main/java/br/com/joseduardo/restaurante/controller/ProdutoController.java
+++ src/main/java/br/com/joseduardo/restaurante/controller/ProdutoController.java
@@ -2,14 +2,16 @@ package br.com.joseduardo.restaurante.controller;
 import Dao.ProdutoDao;
 import model.Categoria;
 import model.Produto;
+import model.dto.ProdutoFormImputDto;
 import model.dto.ProdutoOutputDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,13 +33,18 @@ public class ProdutoController {
     }
 
     @GetMapping("/form")
-    public String form(Model model) {
+    public String form(Model model, ProdutoFormImputDto produtoFormImputDto) {
         model.addAttribute("categorias", Categoria.values());
+        model.addAttribute("produtoFormImputDto", produtoFormImputDto);
         return "form";
     }
 
     @PostMapping("/cadastra")
-    public String cadastra(Produto produto, RedirectAttributes redirectAttributes){
+    public String cadastra(@Valid ProdutoFormImputDto produtoFormImputDto, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model){
+        if(bindingResult.hasErrors())
+            return form(model, produtoFormImputDto);
+
+        Produto produto = produtoFormImputDto.toProduto();
         this.dao.cadastra(produto);
         redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
         return "redirect:/produto/lista";
