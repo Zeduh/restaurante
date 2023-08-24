a5f7ba38a113ea38d729c0882618a17cf6660bed
Zeduh <jeduardocury@gmail.com>
2023-08-24 14:48:39 -0300

    Iniciado implementação de sessão de usuário para página de produtos

--- src/main/java/br/com/joseduardo/restaurante/model/Cliente.java
+++ src/main/java/br/com/joseduardo/restaurante/model/Cliente.java
@@ -2,7 +2,6 @@ package br.com.joseduardo.restaurante.model;
 
 import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
