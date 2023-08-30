66d81bf4d2a68211d3b5fd21730bba5f2d89968c
Zeduh <jeduardocury@gmail.com>
2023-08-30 13:57:47 -0300

    Implementado migrations para criar e popular bando de dados

---
+++ src/main/resources/db/migration/V005__insere-dados.sql
@@ -0,0 +1,18 @@
+insert into produtos(nome, descricao, preco, categoria) values ('produto1', 'descricao1', 101, 'COMIDA');
+insert into produtos(nome, descricao, preco, categoria) values ('produto2', 'descricao2', 102, 'BEBIDA');
+insert into produtos(nome, descricao, preco, categoria) values ('produto3', 'descricao3', 103, 'SOBREMESA');
+insert into produtos(nome, descricao, preco, categoria) values ('produto4', 'descricao4', 104, 'COMIDA');
+insert into produtos(nome, descricao, preco, categoria) values ('produto5', 'descricao5', 105, 'BEBIDA');
+insert into produtos(nome, descricao, preco, categoria) values ('produto6', 'descricao6', 106, 'SOBREMESA');
+
+insert into funcionarios(nome, email, salario) values ('José', 'jose@email.com', 1000);
+insert into funcionarios(nome, email, salario) values ('Guilherme', 'guilherme@email.com', 2000);
+insert into funcionarios(nome, email, salario) values ('Rita', 'rita@email.com', 3000);
+
+insert into clientes(nome, email, senha, ativo) values ('Ana', 'ana@email.com', 1234, true);
+insert into clientes(nome, email, senha, ativo) values ('José', 'jose@email.com', 1234, true);
+insert into clientes(nome, email, senha, ativo) values ('Paula', 'paula@email.com', 1234, true);
+
+insert into enderecos(cep, rua, cliente_id) values ('12170000', 'Primeiro de Dezembro', 2);
+insert into enderecos(cep, rua, cliente_id) values ('18051630', 'Rua dos bobos', 1);
+insert into enderecos(cep, rua, cliente_id) values ('19559-000', 'Alferneiros', 3);
