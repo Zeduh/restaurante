90f6d1d551d18a73cab6ef442f8ef55532fcbf34
Zeduh <jeduardocury@gmail.com>
2023-08-30 15:43:20 -0300

    Finalizado migração de DAO para Repositorys, deletado pacote DAO

--- src/main/java/br/com/joseduardo/restaurante/repository/ClienteRepository.java
+++ src/main/java/br/com/joseduardo/restaurante/repository/ClienteRepository.java
@@ -4,4 +4,7 @@ import br.com.joseduardo.restaurante.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    Cliente findByEmailAndSenha(String email, String senha);
+
 }
--- src/main/java/br/com/joseduardo/restaurante/repository/FuncionarioRepository.java
+++ src/main/java/br/com/joseduardo/restaurante/repository/FuncionarioRepository.java
@@ -3,5 +3,7 @@ package br.com.joseduardo.restaurante.repository;
 import br.com.joseduardo.restaurante.model.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
 }
